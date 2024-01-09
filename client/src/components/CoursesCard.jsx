48e0bec051c90972c79b7a6b3e88b9020f18911b
Costel <costelmartinescu2000@gmail.com>
2024-01-09 11:32:19 +0200

    appearance improvement 2.0

--- client/src/components/CoursesCard.jsx
+++ client/src/components/CoursesCard.jsx
@@ -6,32 +6,36 @@ const CoursesCard = (props) => {
 
     return (
 
-        <div className="card columns-courses mx-auto">
+        <div className="card card-border-courses mx-auto">
 
-            <div className="container-course">
+            <div className="card-body-courses">
 
-                <h4 className="school">{props.school}</h4>
-                <p className="title-course m-0">{props.course}</p>
-                <p>{props.date}</p>
+                <div className="title-card-courses">
 
-            </div>
+                    <h4 className="school">{props.school}</h4>
+                    <p className="title-course">{props.course}</p>
+                    <p>{props.date}</p>
 
-            <div>
-                <p className="description-course">
-                    {props.description}
-                </p>
-            </div>
+                </div>
 
-            <div className="teck">
+                <div>
+                    <p className="content-card-courses">
+                        {props.description}
+                    </p>
+                </div>
 
-                <p className="title-course">I learned</p>
-                <ul>
-                    <li>{teckStack ? teckStack[0] : null}</li>
-                    <li>{teckStack ? teckStack[1] : null}</li>
-                    <li>{teckStack ? teckStack[2] : null}</li>
-                    <li>{teckStack ? teckStack[3] : null}</li>
-                </ul>
+                <div className="teck">
 
+                    <p className="title-course">I learned</p>
+                    <ul>
+                        <li>{teckStack ? teckStack[0] : null}</li>
+                        <li>{teckStack ? teckStack[1] : null}</li>
+                        <li>{teckStack ? teckStack[2] : null}</li>
+                        <li>{teckStack ? teckStack[3] : null}</li>
+                    </ul>
+
+
+                </div>
 
             </div>
 
--- client/src/components/ProjectsCard.jsx
+++ client/src/components/ProjectsCard.jsx
@@ -6,26 +6,40 @@ const ProjectsCard = (props) => {
 
     return (
 
-        <div className="card columns-project mx-auto">
+        <div className="card card-border mx-auto">
 
-            <div className="container-project">
+            <div className="card-body">
 
-                <button className="btn-orange">{props.type}</button>
-                <p className="paragraph">{props.name}</p>
-            </div>
+                <div className="title-card">
 
-            <div>
-                <p className="description">
-                    {props.description}
-                </p>
-            </div>
-            <div className="d-flex justify-content-around align-items-center h-100">
-                <a href={props.code}>
-                    <button type="button" className="btn btn-outline-success">Code</button>
-                </a>
-                <a href={props.visit}>
-                    <button type="button" className="btn btn-success">Visit</button>
-                </a>
+                    <button className="btn-orange">{props.type}</button>
+                    <p className="title-project">{props.name}</p>
+
+                </div>
+
+        
+                    <p className="content-card">
+                        {props.description}
+                    </p>
+           
+
+                <div className="nav-card">
+
+                    <a
+                        href={props.code}
+                        type="button"
+                        className="btn btn-outline-success">
+                        Code
+                    </a>
+                    <a
+                        href={props.visit}
+                        type="button"
+                        className="btn btn-success">
+                        Visit
+
+                    </a>
+
+                </div>
 
             </div>
 
