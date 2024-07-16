a3387e485d283077cf5d64009715d22dda1680f8
Costel <costelmartinescu2000@gmail.com>
2024-07-16 15:35:04 +0300

    update 1.5

--- client/src/analytics.js
+++ client/src/analytics.js
@@ -1,12 +1,35 @@
 import ReactGA from 'react-ga4';
 
+localStorage.setItem('access', false);
+
+let isGAInitialized = sessionStorage.getItem('access');
+
 export const initGA = () => {
-  ReactGA.initialize('G-FCQ1T9WKGC'); 
+  ReactGA.initialize('G-FCQ1T9WKGC');
+  localStorage.setItem('access', true);
+
 };
 
 export const logPageView = (page, title) => {
 
-  ReactGA.send({ hitType: 'pageview', page: page, title: "Custom Title"  });
-  
-  
+  if (isGAInitialized) {
+
+    ReactGA.send({ hitType: 'pageview', page: page, title: title });
+  }
+
 };
+
+export function deleteAnalyticsCookies() {
+  const cookies = document.cookie.split(";");
+
+  cookies.forEach(cookie => {
+    const equalsPosition = cookie.indexOf("=");
+    const name = equalsPosition > -1 ? cookie.substr(0, equalsPosition) : cookie;
+    const trimmedName = name.trim();
+
+    if (trimmedName.startsWith('_ga') || trimmedName.startsWith('_gid') || trimmedName.startsWith('_gat')) {
+
+      document.cookie = `${trimmedName}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+    }
+  });
+}
